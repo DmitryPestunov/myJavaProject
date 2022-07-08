6083a6269d87e760ec2ec6b0c827eeaca088b184
Dmitry Pestunov <PestynOFF@gmail.com>
2022-07-08 23:49:27 +0200

    complete homework 8.1

--- javaLessons/src/main/java/school/lesson8/FindAndShowUniqueWord.java
+++ javaLessons/src/main/java/school/lesson8/FindAndShowUniqueWord.java
@@ -6,6 +6,7 @@ public class FindAndShowUniqueWord {
 
     public void findAndShow(ArrayList<String> list){
         Set<String> unique = new LinkedHashSet<>();
+        Map<String, Integer> countDuplicates = new HashMap<>();
 
         for(String str : list){
             String word = str.toLowerCase();
@@ -14,6 +15,21 @@ public class FindAndShowUniqueWord {
 
         System.out.println("Unique words is " + unique.size());
         System.out.println(unique.toString());
+
+        for(String uniq : unique){
+            String uniqWord = uniq.toLowerCase();
+            int count = 0;
+            for(String str : list){
+                String strWord = str.toLowerCase();
+                if(uniqWord.equals(strWord)){
+                    count++;
+                }
+            }
+            countDuplicates.put(uniq,count);
+        }
+
+        System.out.println(countDuplicates.toString());
+
     }
 
 }
